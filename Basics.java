02bd8b0e30a3c38c61d801c0f26fb58e91661469
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-08-07 11:43:43 +0530

    Variables

--- Basics.java
+++ Basics.java
@@ -1,5 +1,9 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+
 public class Basics {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
+
+// Output: Hello, World!s
---
+++ Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        // create/declare a variable
+        int age;
+
+        // assign a value to the variable
+        age = 25;
+
+        // modify the value of the variable
+        age = 30;
+
+        // print the value of the variable
+        System.out.println("Age: " + age);
+
+        // output: Age: 30
+
+
+        // Note : constants variable names are generally written in uppercase letters with underscores separating words.
+         int DAYS_IN_WEEK = 7;
+         System.out.println("Days in a week: " + DAYS_IN_WEEK);
+
+         // output: Days in a week: 7
+    }
+}
