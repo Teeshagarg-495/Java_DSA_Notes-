d3c6b50201341e26bba7ddfdb2fc8d8622365d71
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-08-25 20:23:54 +0530

    java

---
+++ Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String args[]){
+
+        // for loop to print numbers from 1 to 10
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        // output will be numbers from 1 to 10 printed on separate lines.
+    }
+}
