c92ff5deb301d9992a241ece6641cdefda9ebd73
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-08-26 16:52:34 +0530

    java

--- LoopPractice.java
+++ LoopPractice.java
@@ -42,6 +42,56 @@ public class LoopPractice {
         // Output will be "Teesha" printed 100 times on separate lines.
 
         // print all prime numbers 1 to 100 
+        for (int i = 2; i <= 100; i++){
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++){
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.print(i + " ");
+            }
+        }
+        // Output will be all prime numbers from 1 to 100 printed on the same line separated by spaces.
+
+
+        System.out.println();
+
+        // print all even numbers from 1 to 100
+        for (int i = 2; i <= 100; i+=2){
+                System.out.print(i + " ");
+        }
+
+
+        // Output will be all even numbers from 1 to 100 printed on the same line separated by spaces.
+
+        System.out.println();
+
+        // print the sum of all numbers from 1 to n
+        System.out.print("\nEnter a number: ");
+        int q = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= q; i++){
+            sum += i;
+        }   
+
+        System.out.println("The sum of all numbers from 1 to " + q + " is: " + sum);
+
+        // Output will be the sum of all numbers from 1 to n printed on the same line.
+
+        System.out.println();
+
+        // print all integers in range from 50 to 100, that are perfectly divisible by 7
+        System.out.println("Integers from 50 to 100 divisible by 7:");
+        for (int i = 50; i <= 100; i++){
+            if (i % 7 == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+        // Output will be all integers from 50 to 100 that are perfectly divisible by 7 printed on the same line separated by spaces.
 
 }
 }
