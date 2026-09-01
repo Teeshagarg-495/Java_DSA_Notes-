006c4d3c69fd8b12358af423257395d1689a5e8d
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-09-01 17:01:15 +0530

    java

--- Pattern_Printing_3.java
+++ Pattern_Printing_3.java
@@ -3,8 +3,8 @@ public class Pattern_Printing_3 {
 
         // Pattern 1 : hollow right angled triangle pattern
 
-        for (int i = 1 ; i<=10 ; i++){
-            for (int j = 1 ; j <= 10 ; j++){
+        for (int i = 1 ; i<=5 ; i++){
+            for (int j = 1 ; j <= 5 ; j++){
 
                 if (i == 10 ){
                     System.out.print("* ");
@@ -21,9 +21,41 @@ public class Pattern_Printing_3 {
             System.out.println();
         }
 
+        System.out.println();
 
         // Pattern 2 : Hollow triangle pattern
-        
+
+        for (int i = 1 ; i<= 5 ; i++){
+
+            for(int j = 1 ; j <= 5 ; j ++){
+                if (i==5){
+                    System.out.print("* ");
+                }
+                else {
+                    if (j == 5-i+1 ){
+                        System.out.print("* ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+
+            for (int j = 2 ; j<= i ; j++){
+                if(i ==5){
+                    System.out.print("* ");
+                }
+                else {
+                    if (j == i){
+                        System.out.print("* ");
+                    }
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
