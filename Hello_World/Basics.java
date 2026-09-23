25f5a1b1286e61105632617e9b9b4cf5ea275361
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-09-23 13:48:45 +0530

    java

---
+++ Hello_World/Basics.java
@@ -0,0 +1,11 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// Output: Hello, World!
+
+
