6eab120b6db87bd7790b5f7fa9b3fc136757c44d
Teeshagarg-495 <teeshagarg97@gmail.com>
2026-09-24 21:31:24 +0530

    java

---
+++ 10__Arrays/Array_2D.java
@@ -0,0 +1,19 @@
+public class Array_2D {
+    public static void main(String args[]){
+
+
+        // declarations 
+        int arr[][];
+
+        // allocation
+        arr = new int[3][3];
+
+        // initialization
+        int brr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        System.out.println(brr[0][0]);
+
+
+
+    }
+}
